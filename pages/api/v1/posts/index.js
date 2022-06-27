de1eb9afd9250faf587e0897876d5cac271e2056
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2022-06-27 20:56:17 +0300

    Push portfolio-nextjs

---
+++ pages/api/v1/posts/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+import axios from 'axios';
+
+export default async (_req, res) => {
+  try {
+    const axiosRes = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    const posts = axiosRes.data;
+    res.status(200).json(posts.slice(0, 10));
+  } catch (e) {
+    console.error(e);
+    res.status(e.status || 400).json({ message: 'Api error' });
+  }
+};
