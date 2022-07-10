97c874329647d496b2adfd099287948558254ccc
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2022-07-10 22:48:55 +0300

    Add authentication functionality using Auth0

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
+    config.plugins.push(new Dotenv({ silent: true }));
     return config;
+  },
+  env: {
+    AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE,
   },
 };
 
